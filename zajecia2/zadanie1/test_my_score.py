80e96a6f66b4bf2b42d36af5751d975451dcc6f3
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-03-16 23:41:17 +0100

    dodaj zadanie1

---
+++ zajecia2/zadanie1/test_my_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os
+import math
+
+expected = [float(x.rstrip()) for x in open(
+    os.path.join('dev-0', 'expected.tsv'), 'r').readlines()]
+
+out = [float(x.rstrip()) for x in open(
+    os.path.join('dev-0', 'out.tsv'), 'r').readlines()]
+
+acc = 0
+for x, y in zip(expected, out):
+    if x == y:
+        acc += 1
+
+acc = math.sqrt(acc / len(out))
+
+min_acceptable = 0.85
+
+print('twój wynik acc to: ', round(acc, 2))
+print('żeby zaliczyc zadanie powinieneś mieć conajmniej: ', min_acceptable)
+
+if acc < min_acceptable:
+    print('nie zaliczone')
+else:
+    print('zaliczone')
